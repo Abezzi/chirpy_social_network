e0c99349974f8fe4e2f599133d07440165339baf
abezzi <abezzi@outlook.cl>
2026-05-06 13:02:15 -0400

    feat(endpoint): metrics and reset metrics endpoints

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import { Request, Response, NextFunction } from "express";
+import { apiConfig } from "./config.js";
 
 const app = express();
 const PORT = 8080;
@@ -9,6 +10,7 @@ const handlerReadiness = (req: Request, res: Response) => {
   res.send("OK");
 }
 
+// middleware that logs non-ok
 const middlewareLogging = (req: Request, res: Response, next: NextFunction) => {
   res.on("finish", () => {
     if (res.statusCode !== 200) {
@@ -17,16 +19,44 @@ const middlewareLogging = (req: Request, res: Response, next: NextFunction) => {
   });
   next();
 }
+
+const middlewareMetricsInc = (req: Request, res: Response, next: NextFunction) => {
+  apiConfig.fileserverHits += 1;
+  next();
+}
+
+const handlerMetrics = (req: Request, res: Response) => {
+  res.set("Content-Type", "text/plain");
+  res.send(`Hits: ${apiConfig.fileserverHits}`);
+};
+
+const handlerReset = (req: Request, res: Response) => {
+  apiConfig.fileserverHits = 0;
+  res.set("Content-Type", "text/plain");
+  res.send(`Hits reset to 0`);
+};
+
 // start the server and listen for incoming connections on the specified port
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
 
+// MIDDLEWARES
+
+// increment metrics for fileserver hits
+app.use("/app", middlewareMetricsInc);
 // serve static files from the current directory
 app.use("/app", express.static("./src/app"));
-
-// endpoint to check if the server is ready
-app.get("/healthz", handlerReadiness);
-
-// middleware to log non-ok responses
+// use the middleware handler to log non-ok responses
 app.use(middlewareLogging);
+
+// ENDPOINTS 
+
+// check if the server is ready
+app.get("/api/healthz", handlerReadiness);
+// add 1 to apiConfig.fileserverHits
+app.get("/api/metrics", handlerMetrics);
+// resets to 0 apiConfig.fileserverHits
+app.get("/api/reset", handlerReset);
+
+
