935b43321c9af26c00e80cb1b2f8c7fbd23572a0
abezzi <abezzi@outlook.cl>
2026-05-06 12:01:53 -0400

    feat(middleware): log non-ok responses

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { Request, Response } from "express";
+import { Request, Response, NextFunction } from "express";
 
 const app = express();
 const PORT = 8080;
@@ -9,13 +9,24 @@ const handlerReadiness = (req: Request, res: Response) => {
   res.send("OK");
 }
 
-// serve static files from the current directory
-app.use("/app", express.static("./src/app"));
-
-// end-point to check if the server is ready
-app.get("/healthz", handlerReadiness);
-
+const middlewareLogging = (req: Request, res: Response, next: NextFunction) => {
+  res.on("finish", () => {
+    if (res.statusCode !== 200) {
+      console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`)
+    }
+  });
+  next();
+}
 // start the server and listen for incoming connections on the specified port
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+// serve static files from the current directory
+app.use("/app", express.static("./src/app"));
+
+// endpoint to check if the server is ready
+app.get("/healthz", handlerReadiness);
+
+// middleware to log non-ok responses
+app.use(middlewareLogging);
