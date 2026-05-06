06b08b28f8d4347340c9bf71e8b78ba808673227
abezzi <abezzi@outlook.cl>
2026-05-06 11:46:13 -0400

    feat(readiness): add readiness endpoint
    to check if the server has started succesfully and is listening to
    traffic

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
 import express from "express";
+import { Request, Response } from "express";
 
 const app = express();
+const PORT = 8080;
 
-app.listen(8080);
+const handlerReadiness = (req: Request, res: Response) => {
+  res.set("Content-Type", "text/plain");
+  res.send("OK");
+}
+
+// serve static files from the current directory
+app.use("/app", express.static("./src/app"));
+
+// end-point to check if the server is ready
+app.get("/healthz", handlerReadiness);
+
+// start the server and listen for incoming connections on the specified port
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
